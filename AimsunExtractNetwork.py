27001c998578e86c357b7608e72821e3560b831a
Serena Hughes <sjhughes@berkeley.edu>
2018-06-13 14:03:45 -0700

    should yield coords and speed. if you run script x times it will print turning description x times.

--- AimsunExtractNetwork.py
+++ AimsunExtractNetwork.py
@@ -102,17 +102,14 @@ def ExtractJunctionInformation(model,outputLocation):
                     string = string + str(turnInfSection[k].getId()) + ','
                 string = string + str(turnInfSection[len(turnInfSection) - 1].getId()) + '\n'
                 junctionInfFile.write(string)
+            junctionInfFile.write("\n")
 
             #Write position of junction
             translator=GKCoordinateTranslator(model)
             coords = translator.toDegrees(junctionObj.getPosition())
-            # junctionInfFile.write("Coordinates:\n")
-            # junctionInfFile.write(("%.6f,%.6f\n") % (coords.x, coords.y))
-            polygon = junctionObj.getPolygon()
-            translated = translator.toDegrees(polygon[0])
-            junctionInfFile.write(("%.6f,%.6f\n") % (translated.x, translated.y))
+            junctionInfFile.write("Coordinates:\n")
+            junctionInfFile.write(("%.6f,%.6f\n") % (coords.x, coords.y))
 
-            junctionInfFile.write("\n")
     return 0
 
 def UpdateTurningDescription(numEntranceSections,entranceSections,junctionObj,DefaultAngle):
@@ -294,7 +291,7 @@ def ExtractSectionInformation(model,outputLocation):
             sectionInfFile.write("Speed:\n")
             speed = sectionObj.getSpeed()
             sectionInfFile.write(("%i\n") % speed)
-
+            
             sectionInfFile.write("\n")
     return 0
 
