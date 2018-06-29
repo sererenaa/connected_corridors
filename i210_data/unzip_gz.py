fd6710d9d8d71c8661faf2034b015ad0032536cc
Serena Hughes <sjhughes@berkeley.edu>
2018-06-29 09:02:43 -0700

    no longer hardcoding file names. now, type in folder name and will unzip all .gz files in that folder.

--- i210_data/unzip_gz.py
+++ i210_data/unzip_gz.py
@@ -1,6 +1,7 @@
 import gzip    
 import os
 #for filename in ['probe_data_I210.20171021.waynep1.csv.gz']:
+#TODO: housekeeping. make this into a function. save it not inside the same folder as data.
 
 folder = '10_21_2017'
 for filename in os.listdir(folder):
