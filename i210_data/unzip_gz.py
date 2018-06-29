ca703145b90717be482219b9f3a6555fc5e4c336
Serena Hughes <sjhughes@berkeley.edu>
2018-06-29 11:12:42 -0700

    put into function that can be called on either all files in a folder or only specific files

--- i210_data/unzip_gz.py
+++ i210_data/unzip_gz.py
@@ -1,12 +1,26 @@
 import gzip    
 import os
-#for filename in ['probe_data_I210.20171021.waynep1.csv.gz']:
-#TODO: housekeeping. make this into a function. save it not inside the same folder as data.
 
-folder = '10_21_2017'
-for filename in os.listdir(folder):
-    filename = folder + '/' + filename
-    with gzip.open(filename, 'rt') as f:
-        data = f.read()
-    with open(filename[:-3], 'wt') as f:
-      f.write(data)
+#TODO: save it not inside the same folder as data.
+#TODO: use separator instead of / to allow for use across windows/linux/
+
+def unzip(folder, files=None):
+	"""
+	Unzips all .gz files. Applies to all .gz files in a given folder, unless certain files are specified.
+	Ex/ unzip('10_2017', ['probe_data_I210.20171022.waynep1.csv.gz'])
+	    unzip('10_2017')
+	:param folder: A string of the name of the folder within /data where the files to be unzipped are located.
+	:param files: A list of .gz files to be unzipped.
+	"""
+
+	source = files if files else os.listdir(folder)
+	for filename in (file for file in source if file.endswith('.gz')):
+		if filename.endswith('.gz'):
+		    filename = folder + '/' + filename
+		    with gzip.open(filename, 'rt') as f:
+		        data = f.read()
+		    with open(filename[:-3], 'wt') as f:
+		      f.write(data)
+
+
+unzip('10_2017')
